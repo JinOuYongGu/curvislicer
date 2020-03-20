145b02ed01525fef2b955710b1f03ca1160eb27b
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-03-20 15:57:25 +0100

    Edit bat file

--- curvislice.bat
+++ curvislice.bat
@@ -2,14 +2,12 @@
 
 set gurobi=0
 
-
 set volumic=0
 set nozzle=0.4
 set layer=0.3
 set filament=1.75
 set ironing=0
 set model=
-
 
 set arg=none
 
@@ -39,30 +37,35 @@ echo %model%
 
 echo Generate tetmesh "from %model%.stl" ...
 call toTetmesh.bat %model%
-echo Done!
+echo Tetmesh Called
+pause
 
 echo Optimize...
-
 if "%gurobi%" EQU "1" (
-  .\bin\curvislice_grb.exe %model%.msh -l %layer%
+  .\out\build\x64-Debug\curvislice_grb.exe %model%.msh -l %layer%
 ) else (
-  .\bin\curvislice_osqp.exe %model%.msh -l %layer%
+  .\out\build\x64-Debug\curvislice_osqp.exe %model%.msh -l %layer% --theta 50
 )
-echo Done!
+echo curvislice_osqp Called
+pause
 
 echo Prepare lua for IceSL
 call luaGenerator.bat %model% %volumic% %nozzle% %layer% %filament% %ironing%
-
 if not exist %appdata%\IceSL\icesl-printers\fff\curvi (
   echo Create 'curvi' printer profile for IceSL
   xcopy /S /I /Q /Y .\resources\curvi "%AppData%\IceSL\icesl-printers\fff\curvi"
 )
-
 .\tools\icesl\bin\icesl-slicer.exe settings.lua --service
+echo IceSL called
+pause
 
-.\bin\uncurve.exe -l %layer% --gcode %model%
+.\out\build\x64-Debug\uncurve.exe -l %layer% --gcode %model%
+echo Uncurve called
+pause
 
-cls
+rem cls
+
+call clean.bat
 
 echo "  ______                                  __            __  __                     
 echo " /      \                                /  |          /  |/  |                    
